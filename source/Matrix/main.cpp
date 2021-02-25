30b8f9ae9dcd5457c718b1efa0462e2ab5bcf78c
Vadim Seledets <Vadik446s@gmail.com>
2021-02-25 23:19:13 +0300

    Light up three pixels. Replace all the tabs with spaces.

--- source/Matrix/main.cpp
+++ source/Matrix/main.cpp
@@ -1,128 +1,148 @@
 #include <avr/io.h>
 #include "stdlib.h"
+#include "util/delay.h"
+
+void sleep(long long delayMs)
+{
+    for (long long i = delayMs; i > 0; i--)
+        _delay_ms(1);
+}
 
 const uint8_t NUMBER_OF_PIXELS = 9;
 const uint16_t NUMBER_OF_BYTES = NUMBER_OF_PIXELS * 3;
 
 const uint8_t values[NUMBER_OF_BYTES] = {
-	0, 10, 0,
-	10, 0, 0,
-	0, 0, 10
-};
+    0, 10, 0,
+    10, 0, 0,
+    0, 0, 10};
 
 void show(uint8_t *bytes, uint16_t count)
-{	
-	const uint8_t high = 1;
-	const uint8_t low = 0;
-	
-    volatile uint8_t *ptr = bytes;
-	volatile uint8_t value = *ptr++;
-	volatile uint16_t byte_count = count;
-	volatile uint8_t t1 = low, t2 = high;
-	
-	if (value & 0x80)
-		t1 = high;
-	
-	asm volatile(
-		"start:                             ;   clk        size  \n\t"
-		// bit_7
-		"    out     %[port], %[high]       ; 1 (1)          1   \n\t"
-		"    mov     %[t2], %[high]         ; 2 (1)          1   \n\t"
-		"    out     %[port], %[t1]         ; 3-1 (1)        1   \n\t"
-		"    rjmp    .+0                    ; 5-3 (2)        1   \n\t"
-		"    sbrs    %[value], 6            ; 6-4 (1)        1   \n\t"
-		"    mov     %[t2], %[low]          ; 7-5 (1)        1   \n\t"
-		"    out     %[port], %[low]        ; 7/8-5/6 (1)    1   \n\t"
-		"    rjmp    .+0                    ; 2-7/8 (2)      1   \n\t"
-		// bit 6
-		"    out     %[port], %[high]       ; 1 (1)          1   \n\t"
-		"    mov     %[t1], %[high]         ; 2 (1)          1   \n\t"
-		"    out     %[port], %[t2]         ; 3-1 (1)        1   \n\t"
-		"    rjmp    .+0                    ; 5-3 (2)        1   \n\t"
-		"    sbrs    %[value], 5            ; 6-4 (1)        1   \n\t"
-		"    mov     %[t1], %[low]          ; 7-5 (1)        1   \n\t"
-		"    out     %[port], %[low]        ; 7/8-5/6 (1)    1   \n\t"
-		"    rjmp    .+0                    ; 2-7/8 (2)      1   \n\t"
-		// bit 5
-		"    out     %[port], %[high]       ; 1 (1)          1   \n\t"
-		"    mov     %[t2], %[high]         ; 2 (1)          1   \n\t"
-		"    out     %[port], %[t1]         ; 3-1 (1)        1   \n\t"
-		"    rjmp    .+0                    ; 5-3 (2)        1   \n\t"
-		"    sbrs    %[value], 4            ; 6-4 (1)        1   \n\t"
-		"    mov     %[t2], %[low]          ; 7-5 (1)        1   \n\t"
-		"    out     %[port], %[low]        ; 7/8-5/6 (1)    1   \n\t"
-		"    rjmp    .+0                    ; 2-7/8 (2)      1   \n\t"
-		// bit 4
-		"    out     %[port], %[high]       ; 1 (1)          1   \n\t"
-		"    mov     %[t1], %[high]         ; 2 (1)          1   \n\t"
-		"    out     %[port], %[t2]         ; 3-1 (1)        1   \n\t"
-		"    rjmp    .+0                    ; 5-3 (2)        1   \n\t"
-		"    sbrs    %[value], 3            ; 6-4 (1)        1   \n\t"
-		"    mov     %[t1], %[low]          ; 7-5 (1)        1   \n\t"
-		"    out     %[port], %[low]        ; 7/8-5/6 (1)    1   \n\t"
-		"    rjmp    .+0                    ; 2-7/8 (2)      1   \n\t"
-		// bit 3
-		"    out     %[port], %[high]       ; 1 (1)          1   \n\t"
-		"    mov     %[t2], %[high]         ; 2 (1)          1   \n\t"
-		"    out     %[port], %[t1]         ; 3-1 (1)        1   \n\t"
-		"    rjmp    .+0                    ; 5-3 (2)        1   \n\t"
-		"    sbrs    %[value], 2            ; 6-4 (1)        1   \n\t"
-		"    mov     %[t2], %[low]          ; 7-5 (1)        1   \n\t"
-		"    out     %[port], %[low]        ; 7/8-5/6 (1)    1   \n\t"
-		"    rjmp    .+0                    ; 2-7/8 (2)      1   \n\t"
-		// bit 2
-		"    out     %[port], %[high]       ; 1 (1)          1   \n\t"
-		"    mov     %[t1], %[high]         ; 2 (1)          1   \n\t"
-		"    out     %[port], %[t2]         ; 3-1 (1)        1   \n\t"
-		"    rjmp    .+0                    ; 5-3 (2)        1   \n\t"
-		"    sbrs    %[value], 1            ; 6-4 (1)        1   \n\t"
-		"    mov     %[t1], %[low]          ; 7-5 (1)        1   \n\t"
-		"    out     %[port], %[low]        ; 7/8-5/6 (1)    1   \n\t"
-		"    rjmp    .+0                    ; 2-7/8 (2)      1   \n\t"
-		// bit 1
-		"    out     %[port], %[high]       ; 1 (1)          1   \n\t"
-		"    mov     %[t2], %[high]         ; 2 (1)          1   \n\t"
-		"    out     %[port], %[t1]         ; 3-1 (1)        1   \n\t"
-		"    rjmp    .+0                    ; 5-3 (2)        1   \n\t"
-		"    sbrs    %[value], 0            ; 6-4 (1)        1   \n\t"
-		"    mov     %[t2], %[low]          ; 7-5 (1)        1   \n\t"
-		"    out     %[port], %[low]        ; 7/8-5/6 (1)    1   \n\t"
-		"    sbiw    %[byte_count], 1       ; 2-7/8 (2)      1   \n\t"
-		// bit 0
-		"    out     %[port], %[high]       ; 1 (1)          1   \n\t"
-		"    mov     %[t1], %[high]         ; 2 (1)          1   \n\t"
-		"    out     %[port], %[t2]         ; 3-1 (1)        1   \n\t"
-		"    ld      %[value], %a[ptr]+     ; 5-3 (2)        1   \n\t"
-		"    sbrs    %[value], 7            ; 6-4 (1)        1   \n\t"
-		"    mov     %[t1], %[low]          ; 7-5 (1)        1   \n\t"
-		"    out     %[port], %[low]        ; 7/8-5/6 (1)    1   \n\t"
-		"    brne    start                  ; 2-7/8 (2)      1   \n\t"
+{
+    const uint8_t high = 1;
+    const uint8_t low = 0;
 
-		: [value]      "+r" (value),
-		  [byte_count] "+w" (byte_count),
-		  [t1]         "+r" (t1),
-		  [t2]         "+r" (t2)
-		: [port]	   "I" (_SFR_IO_ADDR(PORTD)),
-		  [ptr]        "e" (ptr),
-		  [high]       "r" (high),
-		  [low]        "r" (low)
-	);
+    volatile uint8_t *ptr = bytes;
+    volatile uint8_t value = *ptr++;
+    volatile uint16_t byte_count = count;
+    volatile uint8_t t1 = low, t2 = high;
+
+    if (value & 0x80)
+        t1 = high;
+
+    asm volatile(
+        "start:                             ;   clk        size  \n\t"
+        // bit_7
+        "    out     %[port], %[high]       ; 1 (1)          1   \n\t"
+        "    mov     %[t2], %[high]         ; 2 (1)          1   \n\t"
+        "    out     %[port], %[t1]         ; 3-1 (1)        1   \n\t"
+        "    rjmp    .+0                    ; 5-3 (2)        1   \n\t"
+        "    sbrs    %[value], 6            ; 6-4 (1)        1   \n\t"
+        "    mov     %[t2], %[low]          ; 7-5 (1)        1   \n\t"
+        "    out     %[port], %[low]        ; 7/8-5/6 (1)    1   \n\t"
+        "    rjmp    .+0                    ; 2-7/8 (2)      1   \n\t"
+        // bit 6
+        "    out     %[port], %[high]       ; 1 (1)          1   \n\t"
+        "    mov     %[t1], %[high]         ; 2 (1)          1   \n\t"
+        "    out     %[port], %[t2]         ; 3-1 (1)        1   \n\t"
+        "    rjmp    .+0                    ; 5-3 (2)        1   \n\t"
+        "    sbrs    %[value], 5            ; 6-4 (1)        1   \n\t"
+        "    mov     %[t1], %[low]          ; 7-5 (1)        1   \n\t"
+        "    out     %[port], %[low]        ; 7/8-5/6 (1)    1   \n\t"
+        "    rjmp    .+0                    ; 2-7/8 (2)      1   \n\t"
+        // bit 5
+        "    out     %[port], %[high]       ; 1 (1)          1   \n\t"
+        "    mov     %[t2], %[high]         ; 2 (1)          1   \n\t"
+        "    out     %[port], %[t1]         ; 3-1 (1)        1   \n\t"
+        "    rjmp    .+0                    ; 5-3 (2)        1   \n\t"
+        "    sbrs    %[value], 4            ; 6-4 (1)        1   \n\t"
+        "    mov     %[t2], %[low]          ; 7-5 (1)        1   \n\t"
+        "    out     %[port], %[low]        ; 7/8-5/6 (1)    1   \n\t"
+        "    rjmp    .+0                    ; 2-7/8 (2)      1   \n\t"
+        // bit 4
+        "    out     %[port], %[high]       ; 1 (1)          1   \n\t"
+        "    mov     %[t1], %[high]         ; 2 (1)          1   \n\t"
+        "    out     %[port], %[t2]         ; 3-1 (1)        1   \n\t"
+        "    rjmp    .+0                    ; 5-3 (2)        1   \n\t"
+        "    sbrs    %[value], 3            ; 6-4 (1)        1   \n\t"
+        "    mov     %[t1], %[low]          ; 7-5 (1)        1   \n\t"
+        "    out     %[port], %[low]        ; 7/8-5/6 (1)    1   \n\t"
+        "    rjmp    .+0                    ; 2-7/8 (2)      1   \n\t"
+        // bit 3
+        "    out     %[port], %[high]       ; 1 (1)          1   \n\t"
+        "    mov     %[t2], %[high]         ; 2 (1)          1   \n\t"
+        "    out     %[port], %[t1]         ; 3-1 (1)        1   \n\t"
+        "    rjmp    .+0                    ; 5-3 (2)        1   \n\t"
+        "    sbrs    %[value], 2            ; 6-4 (1)        1   \n\t"
+        "    mov     %[t2], %[low]          ; 7-5 (1)        1   \n\t"
+        "    out     %[port], %[low]        ; 7/8-5/6 (1)    1   \n\t"
+        "    rjmp    .+0                    ; 2-7/8 (2)      1   \n\t"
+        // bit 2
+        "    out     %[port], %[high]       ; 1 (1)          1   \n\t"
+        "    mov     %[t1], %[high]         ; 2 (1)          1   \n\t"
+        "    out     %[port], %[t2]         ; 3-1 (1)        1   \n\t"
+        "    rjmp    .+0                    ; 5-3 (2)        1   \n\t"
+        "    sbrs    %[value], 1            ; 6-4 (1)        1   \n\t"
+        "    mov     %[t1], %[low]          ; 7-5 (1)        1   \n\t"
+        "    out     %[port], %[low]        ; 7/8-5/6 (1)    1   \n\t"
+        "    rjmp    .+0                    ; 2-7/8 (2)      1   \n\t"
+        // bit 1
+        "    out     %[port], %[high]       ; 1 (1)          1   \n\t"
+        "    mov     %[t2], %[high]         ; 2 (1)          1   \n\t"
+        "    out     %[port], %[t1]         ; 3-1 (1)        1   \n\t"
+        "    rjmp    .+0                    ; 5-3 (2)        1   \n\t"
+        "    sbrs    %[value], 0            ; 6-4 (1)        1   \n\t"
+        "    mov     %[t2], %[low]          ; 7-5 (1)        1   \n\t"
+        "    out     %[port], %[low]        ; 7/8-5/6 (1)    1   \n\t"
+        "    sbiw    %[byte_count], 1       ; 2-7/8 (2)      1   \n\t"
+        // bit 0
+        "    out     %[port], %[high]       ; 1 (1)          1   \n\t"
+        "    mov     %[t1], %[high]         ; 2 (1)          1   \n\t"
+        "    out     %[port], %[t2]         ; 3-1 (1)        1   \n\t"
+        "    ld      %[value], %a[ptr]+     ; 5-3 (2)        1   \n\t"
+        "    sbrs    %[value], 7            ; 6-4 (1)        1   \n\t"
+        "    mov     %[t1], %[low]          ; 7-5 (1)        1   \n\t"
+        "    out     %[port], %[low]        ; 7/8-5/6 (1)    1   \n\t"
+        "    brne    start                  ; 2-7/8 (2)      1   \n\t"
+
+        : [byte_count] "+w"(byte_count),
+          [value]      "+r"(value),
+          [t1]         "+r"(t1),
+          [t2]         "+r"(t2)
+        : [port]       "I"(_SFR_IO_ADDR(PORTD)),
+          [ptr]        "e"(ptr),
+          [high]       "r"(high),
+          [low]        "r"(low));
 }
 
 int main(void)
 {
-	// Matrix initialization
-	
-	DDRD = 0x01;
-	PORTD = 0x00;
-	
-	uint8_t *bytes = (uint8_t *)malloc(NUMBER_OF_BYTES * sizeof(uint8_t));
-	for (uint8_t i = 0; i < NUMBER_OF_BYTES; ++i)
-		bytes[i] = values[i];
-	
-	show(bytes, NUMBER_OF_BYTES);
-	
-    while (1) 
+    // Matrix initialization
+
+    DDRD = 0x01;
+    PORTD = 0x00;
+
+    uint8_t *bytes = (uint8_t *)malloc(NUMBER_OF_BYTES * sizeof(uint8_t));
+    for (uint8_t i = 0; i < NUMBER_OF_BYTES; ++i)
+        bytes[i] = values[i];
+
+    while (1)
     {
+        sleep(300);
+        for (uint8_t i = 0; i < 255; ++i)
+        {
+            bytes[1] = i;
+            bytes[3] = i;
+            bytes[8] = i;
+            show(bytes, NUMBER_OF_BYTES);
+            sleep(50);
+        }
+        for (uint8_t i = 255; i > 0; --i)
+        {
+            bytes[1] = i;
+            bytes[3] = i;
+            bytes[8] = i;
+            show(bytes, NUMBER_OF_BYTES);
+            sleep(50);
+        }
     }
 }
