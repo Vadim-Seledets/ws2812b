ef35db44961d758d801077011e2f4f4181e61aa4
Vadim Seledets <Vadik446s@gmail.com>
2021-03-04 22:55:10 +0300

    Light up a chosen led

--- source/Matrix/main.cpp
+++ source/Matrix/main.cpp
@@ -20,6 +20,9 @@ ISR (TIMER0_COMP_vect)
 
 void init()
 {
+	DDRB = 0x00;
+	PORTB = 0x00;
+	
     DDRD = 0x01;
     PORTD = 0x00;
 
@@ -29,6 +32,7 @@ void init()
 	TIMSK = 0x02; // Enable output compare match interrupt
 	OCR0 = 0x81;
     
+	clear();
 	sei();
 }
 
@@ -36,33 +40,68 @@ int main(void)
 {
     init();
 
-	clear();
+    uint8_t x = MATRIX_WIDTH / 2;
+    uint8_t y = MATRIX_HEIGHT / 2;
+
+	set_pixel(x, y, {ColorModel::RGB, {30, 0, 0}});
 
     while (1)
     {
-        for (uint8_t x = 0; x < MATRIX_WIDTH - 1; ++x)
+        switch (PINB)
         {
-            clear();
-            set_pixel(x, 0, {ColorModel::RGB, {30, 0, 0}});
-            _delay_ms(60);
+            case 1:
+                clear();
+                if (x < MATRIX_WIDTH - 1)
+                    x++;
+                set_pixel(x, y, {ColorModel::RGB, {30, 0, 0}});
+                _delay_ms(150);
+                break;
+            case 2:
+                clear();
+                if (y > 0)
+                    y--;
+                set_pixel(x, y, {ColorModel::RGB, {30, 0, 0}});
+                _delay_ms(150);
+                break;
+            case 4:
+                clear();
+                if (y < MATRIX_HEIGHT - 1)
+                    y++;
+                set_pixel(x, y, {ColorModel::RGB, {30, 0, 0}});
+                _delay_ms(150);
+                break;
+            case 8:
+                clear();
+                if (x > 0)
+                    x--;
+                set_pixel(x, y, {ColorModel::RGB, {30, 0, 0}});
+                _delay_ms(150);
+                break;
         }
-        for (uint8_t y = 0; y < MATRIX_HEIGHT; ++y)
-        {
-            clear();
-            set_pixel(MATRIX_WIDTH - 1, y, {ColorModel::RGB, {0, 30, 0}});
-            _delay_ms(60);
-        }
-        for (uint8_t x = 14; x > 0; --x)
-        {
-            clear();
-            set_pixel(x, MATRIX_HEIGHT - 1, {ColorModel::RGB, {0, 0, 30}});
-            _delay_ms(60);
-        }
-        for (uint8_t y = 15; y > 0; --y)
-        {
-            clear();
-            set_pixel(0, y, {ColorModel::RGB, {30, 30, 30}});
-            _delay_ms(60);
-        }
+
+        // for (uint8_t x = 0; x < MATRIX_WIDTH - 1; ++x)
+        // {
+        //     clear();
+        //     set_pixel(x, 0, {ColorModel::RGB, {30, 0, 0}});
+        //     _delay_ms(30);
+        // }
+        // for (uint8_t y = 0; y < MATRIX_HEIGHT; ++y)
+        // {
+        //     clear();
+        //     set_pixel(MATRIX_WIDTH - 1, y, {ColorModel::RGB, {0, 30, 0}});
+        //     _delay_ms(30);
+        // }
+        // for (uint8_t x = 14; x > 0; --x)
+        // {
+        //     clear();
+        //     set_pixel(x, MATRIX_HEIGHT - 1, {ColorModel::RGB, {0, 0, 30}});
+        //     _delay_ms(30);
+        // }
+        // for (uint8_t y = 15; y > 0; --y)
+        // {
+        //     clear();
+        //     set_pixel(0, y, {ColorModel::RGB, {30, 30, 30}});
+        //     _delay_ms(30);
+        // }
     }
 }
